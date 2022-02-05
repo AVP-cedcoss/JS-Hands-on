8cf9a49f96fc719e077a2d263f623b62086bb7ff
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-02-05 15:05:55 +0530

    Task 93

--- src/script.js
+++ src/script.js
@@ -1,14 +1,13 @@
-const myMusic = [
-  {
-    "artist": "Billy Joel",
-    "title": "Piano Man",
-    "release_year": 1973,
-    "formats": ["CD","8T","LP"]
-  },
-    {
-    "artist": "Anugrah",
-    "title": "Piano Man",
-    "release_year": 2000,
-    "formats": ["CD","8T","LP"]
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
   }
-];
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"];
