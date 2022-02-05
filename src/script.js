fd590160785ba393d88104800c4a7d8b24fe7731
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-02-05 14:10:12 +0530

    Task 78

--- src/script.js
+++ src/script.js
@@ -1,25 +1,27 @@
-function caseInSwitch(val) {
+function sequentialSizes(val) {
     let answer = "";
     // Only change code below this line
     switch (val)
     {
-      case 1: answer = "alpha";
-      break;
-  
-      case 2: answer = "beta";
-      break;
-  
-      case 3: answer = "gamma";
-      break;
-  
-      case 4: answer = "delta";
-      break;
-
-      default: answer = "theta";
+      case 1:
+      case 2:
+      case 3:
+        answer = "Low";
+        break;
+      case 4:
+      case 5:
+      case 6:
+        answer = "Mid";
+        break;
+      case 7:
+      case 8:
+      case 9:
+        answer = "High";
+        break;
     }
   
     // Only change code above this line
     return answer;
   }
   
-  caseInSwitch(1);
+  sequentialSizes(1);
