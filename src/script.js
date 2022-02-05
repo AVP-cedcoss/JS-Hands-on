d9ec7ce82d3e41f3f3fddb642a859aa6173bd0d6
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-02-05 12:59:01 +0530

    Task 56

--- src/script.js
+++ src/script.js
@@ -1,17 +1,17 @@
 // Setup
-let sum = 0;
+let processed = 0;
 
-function addThree() {
-  sum = sum + 3;
+function processArg(num) {
+  return (num + 3) / 5;
 }
 
 // Only change code below this line
-function addFive()
-{
-  sum += 5;
+processed = processArg(7);// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
 }
 
-// Only change code above this line
-
-addThree();
-addFive();
+// Only change code below this line
+processed = processArg(7);
