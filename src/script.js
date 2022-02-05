70c7107d5b81249ccdeb6790fcd0dad0e6f97099
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-02-05 11:46:12 +0530

    Task 31

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
-// Only change code below this line
-const myName = "Anugrah Vishwas Paul";
-const myStr = "My name is" + myName + "and I am well!";
+// Change code below this line
+const someAdjective = "Awesome";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+console.log(myStr);
