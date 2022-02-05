722b6d93f9cfbcca27f4e1269636241a1f1e4f15
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-02-05 15:23:21 +0530

    Task 99

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 var ourArray = [];
 
-for (var i = 0; i < 10; i += 2) {
+for (var i = 10; i > 0; i -= 2) {
   ourArray.push(i);
 }
 
@@ -8,7 +8,6 @@ for (var i = 0; i < 10; i += 2) {
 var myArray = [];
 
 // Only change code below this line.
-
-for (var i = 1; i < 10; i += 2) {
+for (var i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }
