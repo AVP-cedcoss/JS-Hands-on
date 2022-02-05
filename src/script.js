f7253260b0cbf8b54ebf4cbb84b55e0bfa921a2f
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-02-05 14:31:26 +0530

    Task 83

--- src/script.js
+++ src/script.js
@@ -1,29 +1,17 @@
-let count = 0;
-
-function cc(card) {
+const myDog = {
   // Only change code below this line
-  switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-      break;
-  }
-  if (count > 0) {
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
-  // Only change code above this line
-}
+  name: "Tommy",
+  legs: 4,
+  tails: 1,
+  friends: ["maggi","jack"]
 
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+  // Only change code above this line
+};const myDog = {
+  // Only change code below this line
+  name: "Tommy",
+  legs: 4,
+  tails: 1,
+  friends: ["maggi","jack"]
+
+  // Only change code above this line
+};
