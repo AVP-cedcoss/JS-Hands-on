0efe454911e2d6e44fdd074fcc93155d379b2cce
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-02-05 12:34:22 +0530

    Task 48

--- src/script.js
+++ src/script.js
@@ -1,6 +1,1 @@
-// Setup
-const myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-
-// Only change code below this line
-myArray.unshift(["Paul", 35]);
+const myList = [["Choclate", 10], ["Detergent", 5], ["Soap", 10], ["Toothpaste", 3], ["Coffee", 5]];
