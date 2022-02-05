e442819ae3f8509c3884e1cf0b3dcbb54c2845f6
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-02-05 12:13:44 +0530

    Task 41

--- src/script.js
+++ src/script.js
@@ -1,2 +1,2 @@
-// Only change code below this line
-const myArray = [["i3 9100F","GT 710"], ["i3 10100F", "GtX 1050Ti"], ["i7 10700K", "RTX 3080"]];
+const myArray = [50, 60, 70];
+var myData  = myArray[0];
